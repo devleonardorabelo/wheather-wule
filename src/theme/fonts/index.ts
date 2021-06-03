3ebf21f4470196a7ef4eff18449a0b8d71a02cda
Leonardo Rabelo <devleonardorabelo@gmail.com>
2021-06-03 20:59:26 -0300

    feat: added font styles

--- src/theme/fonts/index.ts
+++ src/theme/fonts/index.ts
@@ -1,5 +1,32 @@
-import {StyleSheet} from 'react-native';
-
-const FONTS = StyleSheet.create({});
+const FONTS = {
+  h1: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 48,
+    lineHeight: 56,
+  },
+  h2: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 24,
+    lineHeight: 28,
+  },
+  p: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    fontSize: 18,
+    lineHeight: 21,
+  },
+  strong: {
+    fontFamily: 'Roboto',
+    fontStyle: 'normal',
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 21,
+  },
+};
 
 export default FONTS;
