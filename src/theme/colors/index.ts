009a6f43398997ef79d66b746cce82b51e009229
Leonardo Rabelo <devleonardorabelo@gmail.com>
2021-06-03 20:30:44 -0300

    chore: project started

---
+++ src/theme/colors/index.ts
@@ -0,0 +1,17 @@
+const COLORS = {
+  BLUE: {
+    DARK: '#1F5887',
+    MEDIUM: '#1F7587',
+    NORMAL: '#3594ED',
+    LIGHT: '#35D1ED',
+  },
+  YELLOW: {
+    MEDIUM: '#ECC438',
+  },
+  BLACK: {
+    DARK: '#081E24',
+    MEDIUM: '#0D2639',
+  },
+};
+
+export default COLORS;
