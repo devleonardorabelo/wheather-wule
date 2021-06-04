7000e290443cad3ea750ce0a832451a9ca7902ca
Leonardo Rabelo <devleonardorabelo@gmail.com>
2021-06-03 21:12:17 -0300

    refactor: organized image exports

---
+++ src/assets/index.js
@@ -0,0 +1,19 @@
+export const IMAGES = {
+  moonCloud: require('./images/moon_cloud_big.png'),
+  night: require('./images/night_big'),
+  rainMoon: require('./images/rain_with_moon_big.png'),
+  rainSun: require('./images/rain_with_soon_big.png'),
+  rainy: require('./images/rainy_big.png'),
+  sun: require('./images/sun_big.png'),
+  sunCloud: require('./images/sun_cloud_big.png'),
+};
+
+export const ICONS = {
+  moonCloud: require('./images/moon_cloud.png'),
+  night: require('./images/night'),
+  rainMoon: require('./images/rain_with_moon.png'),
+  rainSun: require('./images/rain_with_soon.png'),
+  rainy: require('./images/rainy.png'),
+  sun: require('./images/sun.png'),
+  sunCloud: require('./images/sun_cloud.png'),
+};
