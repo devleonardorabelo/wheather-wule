643f03e69132dbcd877e93454fbf27c57f04ea11
Leonardo Rabelo <devleonardorabelo@gmail.com>
2021-06-06 21:10:03 -0300

    refactor: removed unnecessary images

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,24 +1,10 @@
-export const IMAGES = {
-  moonCloud: require('./images/moon_cloud_big.png'),
-  night: require('./images/night_big.png'),
-  rainMoon: require('./images/rain_with_moon_big.png'),
-  rainSun: require('./images/rain_with_moon_big.png'),
-  rainy: require('./images/rainy_big.png'),
-  sun: require('./images/sun_big.png'),
-  sunCloud: require('./images/sun_cloud_big.png'),
-};
-
 export const ICONS = {
-  moonCloud: require('./icons/moon_cloud.png'),
-  night: require('./icons/night.png'),
-  rainMoon: require('./icons/rain_with_moon.png'),
-  rainSun: require('./icons/rain_with_moon.png'),
-  rainy: require('./icons/rainy.png'),
-  sun: require('./icons/sun.png'),
-  sunCloud: require('./icons/sun_cloud.png'),
   settings: require('./icons/settings.png'),
   arrowLeft: require('./icons/arrow_left.png'),
   arrowRight: require('./icons/arrow_right.png'),
   pin: require('./icons/pin.png'),
   humidity: require('./icons/humidity.png'),
+  tempMin: require('./icons/temp_min.png'),
+  tempMax: require('./icons/temp_max.png'),
+  dayHumidity: require('./icons/day_humidity.png'),
 };
